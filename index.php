8ca14b7d7cf4f825e81488c55ad15b3bf4fb0117
Nataly377 <bogdanovicnatasa2@gmail.com>
2025-02-14 22:29:37 +0100

    expiriance

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Relvise1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 
@@ -15,67 +16,109 @@
 
 <nav class="navbar navbar-expand-lg">
   <div class="container">
-    <a class="navbar-brand" href="#">Relvise</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
+        <a class="navbar-brand" href="#">Relvise</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
         <a class="nav-link me-3" aria-current="page" href="#">Home</a>
         <a class="nav-link me-4" href="#">Product</a>
         <a class="nav-link me-3" href="#">Pricing</a>
         <a class="nav-link me-3" aria-disabled="true">Contact</a>
-      </div>
+     </div>
     </div>
   </div>
 </nav>
 
+  
+<!-- header -->
+
 <div class="cover">
- <div class="container">
-  <div class="naslov">
-    <h1>Finance and Consultancy Solution</h1>
-    <p>We know how large objects will act, things on a small scale.</p>
+  <div class="container">
+    <div class="naslov">
+      <h1 class="mb-2">Finance and Consultancy Solution</h1>
+      <h4 class="mt-3 mb-5">We know how large objects will act, things on a small scale.</h4>
     <div>
-      <a href="" class="btn btn-text me-3">Get Quote Now</a>
-      <a href="" class="btn btn-text2">Learn More</a>
+       <a href="" class="btn btn-text me-3">Get Quote Now</a>
+       <a href="" class="btn btn-text2">Learn More</a>
     </div>
-  </div> 
+   </div> 
   </div>
 </div>
 
 <article class="kartica container my-5">
   <div class="row">
-    <div class="col-md-4">
-      <div class="card border-light mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
+    <div class="col-md-3">
+      <div class="card border-light mb-4 shadow" style="max-width: 18rem;">
+        <div class="py-3 px-3">
+          <svg xmlns="http://www.w3.org/2000/svg" width="48" height="40" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16">
+          <path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5m1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0M1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5"/>
+          </svg>
+        </div>
         <div class="card-body">
-          <h5 class="card-title">Light card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <h3 class="card-title">Environmental Consulting</h3>
+           <p class="card-text">We focus on ergonomics and 
+            meeting you where you work. </p>
         </div>
       </div>
     </div>
 
-    <div class="col-md-4">
-      <div class="card border-light mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-          <h5 class="card-title">Light card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
+  <div class="col-md-3">
+    <div class="card border-light mb-4 shadow " style="max-width: 18rem;">
+      <div class="py-3 px-3">
+         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="40" fill="currentColor" class="bi bi-shop" viewBox="0 0 16 16">
+         <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.37 2.37 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0M1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5M4 15h3v-5H4zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1zm3 0h-2v3h2z"/>
+         </svg>
+      </div>
+      <div class="card-body">
+         <h3 class="card-title">Finance and 
+         consultancy </h3>
+         <p class="card-text">Just type what's on your mind 
+         and we'll get you there.</p>
       </div>
     </div>
+  </div>
 
-    <div class="col-md-4">
-      <div class="card border-light mb-3" style="max-width: 18rem;">
-        <div class="card-header">Header</div>
-        <div class="card-body">
-          <h5 class="card-title">Light card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <div class="col-md-3">
+    <div class="card border-light mb-4 shadow" style="max-width: 18rem;">
+      <div class="py-3 px-3">
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="40" fill="currentColor" class="bi bi-book-half" viewBox="0 0 16 16">
+        <path d="M8.5 2.687c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+        </svg>
+      </div>
+      <div class="card-body">
+          <h3 class="card-title">Financial Services 
+          Consulting</h3>
+          <p class="card-text">the quick fox jumps over the 
+          lazy dog</p>
         </div>
       </div>
     </div>
   </div>
 </article>
+
+<article class="exp">
+  <div class="text-center">
+    <h2>Designing Better Experience</h2>
+    <p>Problems trying to resolve the conflict between the two major realms 
+    of Classical physics: Newtonian mechanics </p>
+  </div>
+
+  <div class="container col-md-3 text-center py-5">
+    <div class="exp border-light mb-4 shadow py-4 " style="max-width: 15rem;">
+      <div class="py-3 px-3">
+      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="44" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+  <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+</svg>
+      </div>
+      <div>
+         <h2>972 +</h2>
+         <h5>Consumer Products </h5>
+      </div>
+    </div>
+  </div>
+</article>  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
